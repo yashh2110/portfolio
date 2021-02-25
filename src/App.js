6cc4a6900ebbdd3541c276ef42b1a1566e955fdf
yashh2110 <77020506+yashh2110@users.noreply.github.com>
2021-02-25 16:08:57 +0530

    w

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import React,{useEffect,useRef} from 'react'
+import React from 'react'
 import Loader from './components/Loader'
 import Main from "./components/Main";
-import gsap from "gsap";
 import "./css/app.css";
 import Nav from './components/Nav';
 function App() {
-    const curser= useRef(null);
-    const curser_ref= useRef(null);
-    
-    useEffect(() =>{
-        window.addEventListener("mousemove",(e)=>{
-            // curser.current.style.left=e.clientX + "px";
-            // // curser.current.style.top=e.clientY + "px";
-            // curser_ref.current.style.left=e.clientX + "px";
-            // curser_ref.current.style.top=e.clientY + "px";
-        })
-    })
     return (
         <div className="app">
             <Loader/>
