b7d9a712455e8abaace6df56ae0437e768a182a1
yashh2110 <77020506+yashh2110@users.noreply.github.com>
2021-02-25 19:06:52 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Loader from './components/Loader'
 import Main from "./components/Main";
 import "./css/app.css";
 import Nav from './components/Nav';
 import Social from './components/Social';
 function App() {
+    useEffect(()=>{
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    })
     return (
         <div className="app">
             <Loader/>
